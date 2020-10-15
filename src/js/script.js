83031a11c8d8aa6f3109a50a0a98fc4bf9322ff4
Tien <tien.ru@mail.ru>
2020-10-15 06:32:27 +0300

    3

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
